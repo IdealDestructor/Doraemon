8365af94d54e3c50b69c4db2be2f65429c843161
IdealDestructor <wdqjj@qq.com>
2020-04-28 10:26:47 +0800

    fix

--- assets/js/common.js
+++ assets/js/common.js
@@ -20,55 +20,55 @@ $(document).ready(function () {
 $('.ui.inverted.masthead.segment').addClass('bg' + Math.ceil(Math.random() * 14)).removeClass('zoomed');
 
 // analytics
-$.getJSON(analyticsAPI.url, {
-    'module': 'API',
-    'method': 'VisitsSummary.getVisits',
-    'idSite': analyticsAPI.id,
-    'period': 'day',
-    'date': 'yesterday',
-    'format': 'JSON',
-    'token_auth': analyticsAPI.token
-}, function (data) {
-    $('#yesterday-visits').text(data.value);
-});
-$.getJSON(analyticsAPI.url, {
-    'module': 'API',
-    'method': 'VisitsSummary.getActions',
-    'idSite': analyticsAPI.id,
-    'period': 'day',
-    'date': 'yesterday',
-    'format': 'JSON',
-    'token_auth': analyticsAPI.token
-}, function (data) {
-    $('#yesterday-actions').text(data.value);
-});
-function updateAnalytics() {
-    $.getJSON(analyticsAPI.url, {
-        'module': 'API',
-        'method': 'VisitsSummary.getVisits',
-        'idSite': analyticsAPI.id,
-        'period': 'day',
-        'date': 'today',
-        'format': 'JSON',
-        'token_auth': analyticsAPI.token
-    }, function (data) {
-        $('#today-visits').text(data.value);
-    });
-    $.getJSON(analyticsAPI.url, {
-        'module': 'API',
-        'method': 'VisitsSummary.getActions',
-        'idSite': analyticsAPI.id,
-        'period': 'day',
-        'date': 'today',
-        'format': 'JSON',
-        'token_auth': analyticsAPI.token
-    }, function (data) {
-        $('#today-actions').text(data.value);
-    });
-};
+// $.getJSON(analyticsAPI.url, {
+//     'module': 'API',
+//     'method': 'VisitsSummary.getVisits',
+//     'idSite': analyticsAPI.id,
+//     'period': 'day',
+//     'date': 'yesterday',
+//     'format': 'JSON',
+//     'token_auth': analyticsAPI.token
+// }, function (data) {
+//     $('#yesterday-visits').text(data.value);
+// });
+// $.getJSON(analyticsAPI.url, {
+//     'module': 'API',
+//     'method': 'VisitsSummary.getActions',
+//     'idSite': analyticsAPI.id,
+//     'period': 'day',
+//     'date': 'yesterday',
+//     'format': 'JSON',
+//     'token_auth': analyticsAPI.token
+// }, function (data) {
+//     $('#yesterday-actions').text(data.value);
+// });
+// function updateAnalytics() {
+//     $.getJSON(analyticsAPI.url, {
+//         'module': 'API',
+//         'method': 'VisitsSummary.getVisits',
+//         'idSite': analyticsAPI.id,
+//         'period': 'day',
+//         'date': 'today',
+//         'format': 'JSON',
+//         'token_auth': analyticsAPI.token
+//     }, function (data) {
+//         $('#today-visits').text(data.value);
+//     });
+//     $.getJSON(analyticsAPI.url, {
+//         'module': 'API',
+//         'method': 'VisitsSummary.getActions',
+//         'idSite': analyticsAPI.id,
+//         'period': 'day',
+//         'date': 'today',
+//         'format': 'JSON',
+//         'token_auth': analyticsAPI.token
+//     }, function (data) {
+//         $('#today-actions').text(data.value);
+//     });
+// };
 //updateAnalytics();
-setInterval(function () {
-    if (!document.hidden) {
-        updateAnalytics();
-    };
-}, 60000);
+// setInterval(function () {
+//     if (!document.hidden) {
+//         updateAnalytics();
+//     };
+// }, 60000);
